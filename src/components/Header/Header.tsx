3c899a233ee5a5ac4f2a5f436ec4d3b5e1c8646b
Nikita K <rabot422@gmail.com>
2024-02-13 18:34:47 +0300

    add and style filter button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import s from './Header.module.css';
-
+import FILTER_IMG from "../../assets/icons/filter.png"
 export const Header = () => {
   return (
     <header className={s.wrap}>
       <h1 className={s.title}>Search</h1>
+      <div className={s.input_wrap}>
       <input placeholder="Write name, tag, email..." type="text" className={s.input} />
+        <button className={s.filter_btn}><img src={FILTER_IMG} alt="Filter" /></button>
+      </div>
       <nav className={s.nav}>
         <ul className={s.list}>
-          <li className={s.list_item}>All</li>
+          <li className={s.list_item}>
+            All 
+            </li>
           <li className={s.list_item}>Designers</li>
           <li className={s.list_item}>Analysts</li>
           <li className={s.list_item}>Managers</li>
